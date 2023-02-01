e86f241d276393cbb67d71396733b3666c17957c
Michelle <choi.michelle.12@gmail.com>
2023-02-01 10:32:14 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(data){
+    let matchingElement = data.find(e => e.result === "W");
+    if (matchingElement){
+        // console.log(matchingElement.year)
+        return matchingElement.year;
+    }
+    else {return matchingElement};
+    // console.log(year)
+
+}
